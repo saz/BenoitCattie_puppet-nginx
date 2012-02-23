487750639ffc95a2a4ced2be8c5ac636ab57842a
Steffen Zieger <me@saz.sh>
2012-02-23 19:18:39 +0100

    changes I've made a looong time ago...

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,27 +1,41 @@
-# Define: nginx::site
-#
-# Install a nginx site in /etc/nginx/sites-available (and symlink in /etc/nginx/sites-enabled).
-#
-#
-# Parameters :
-# * ensure: typically set to "present" or "absent". Defaults to "present"
-# * content: site definition (should be a template).
-#
-define nginx::site($ensure='present', $content='') {
-  case $ensure {
-    'present' : {
-      nginx::install_site { $name:
-        content => $content
-      }
-    }
-    'absent' : {
-      exec { "/bin/rm -f /etc/nginx/sites-enabled/${name}":
-        onlyif  => "/bin/sh -c '[ -L /etc/nginx/sites-enabled/${name} ] && \
-          [ /etc/nginx/sites-enabled/$name -ef /etc/nginx/sites-available/${name} ]'",
-        notify  => Exec['reload-nginx'],
-        require => Package['nginx'],
-      }
-    }
-    default: { err ("Unknown ensure value: '$ensure'") }
-  }
+define nginx::site ( $ensure = 'present', $content = '', $source = '' ) {
+	include nginx
+
+if $content != '' {
+	file { "/etc/nginx/sites-available/${name}":
+		ensure 	=> $ensure,
+		content => $content,
+		mode 	=> 644,
+		owner 	=> root,
+		group 	=> root,
+		notify 	=> Service["nginx"],
+		require	=> Package["nginx"],
+	}
+} else {
+	file { "/etc/nginx/sites-available/${name}":
+		ensure 	=> $ensure,
+		source => $source,
+		mode 	=> 644,
+		owner 	=> root,
+		group 	=> root,
+		notify 	=> Service["nginx"],
+		require	=> Package["nginx"],
+	}
 }
+	
+
+	case $ensure {
+		'present': {
+			file { "/etc/nginx/sites-enabled/${name}":
+				ensure => link,
+				target => "/etc/nginx/sites-available/${name}",
+			}
+		}
+		'absent': {
+			file { "/etc/nginx/sites-enabled/${name}":
+				ensure => absent,
+			}
+		}
+	}
+}
+
